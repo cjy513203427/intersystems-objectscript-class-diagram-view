25eaa85b655768344fd318ed7b15f77843b28692
cjy513203427 <cjy513203427@gmail.com>
2025-01-22 14:44:53 +0100

    change name of plugin
    add logic for scanning project directories and extract classname, propoerties, methods
    
    add generating uml files

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,94 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('intersystems-objectscript-class-diagram-view.generateClassDiagram', () => {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders) {
+	  vscode.window.showInformationMessage('Scanning folders');
+      const rootPath = workspaceFolders[0].uri.fsPath;
+      scanDirectory(rootPath);
+    } else {
+      vscode.window.showInformationMessage('No workspace folder open');
+    }
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "intersystems-objectscript-class-diagram-view" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('intersystems-objectscript-class-diagram-view.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from InterSystems ObjectScript Class Diagram View!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function scanDirectory(dir: string) {
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        if (stats.isDirectory()) {
+          scanDirectory(filePath);
+        } else if (filePath.endsWith('.cls')) {
+          parseObjectScriptFile(filePath);
+        }
+      });
+    });
+  });
+}
+
+function parseObjectScriptFile(filePath: string) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const className = extractClassName(data);
+    const attributes = extractAttributes(data);
+    const methods = extractMethods(data);
+    generateUmlFile(filePath, className, attributes, methods);
+  });
+}
+
+function extractClassName(data: string): string {
+  const match = data.match(/Class ([\w.]+)/);
+  if (match) {
+    const classNameParts = match[1].split('.');
+    return classNameParts[classNameParts.length - 1];
+  }
+  return 'UnknownClass';
+}
+
+function extractAttributes(data: string): string[] {
+  const matches = data.match(/Property (\w+)/g);
+  return matches ? matches.map(m => m.split(' ')[1]) : [];
+}
+
+function extractMethods(data: string): string[] {
+  const matches = data.match(/Method (\w+)/g);
+  return matches ? matches.map(m => m.split(' ')[1]) : [];
+}
+
+function generateUmlFile(filePath: string, className: string, attributes: string[], methods: string[]) {
+  const umlContent = `
+@startuml
+class ${className} {
+  ${attributes.map(attr => `+ ${attr}`).join('\n  ')}
+  ${methods.map(method => `+ ${method}()`).join('\n  ')}
+}
+@enduml
+  `;
+  const umlFilePath = filePath.replace('.cls', '.puml');
+  fs.writeFile(umlFilePath, umlContent, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      vscode.window.showInformationMessage(`UML file generated: ${umlFilePath}`);
+    }
+  });
+}
+
 export function deactivate() {}
