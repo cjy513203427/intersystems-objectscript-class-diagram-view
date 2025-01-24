621f74204e036ed2668d18fdfb25cf4af7cceee8
cjy513203427 <cjy513203427@gmail.com>
2025-01-24 09:31:44 +0100

    add extraction for library datatype

--- src/extractClassInfo.ts
+++ src/extractClassInfo.ts
@@ -19,17 +19,30 @@ export function extractSuperClasses(data: string): string[] {
 }
 
 export function extractAttributes(data: string): string[] {
-  const propertyMatches = data.match(/Property (\w+) As ([\w.]+)/g);
+  const propertyMatches = data.match(/Property (\w+) As ([\w.%()]+)/g);
   const parameterMatches = data.match(/Parameter (\w+) = "([^"]*)"/g);
+  const indexMatches = data.match(/Index (\w+) On (\w+)/g);
+  
   const properties = propertyMatches ? propertyMatches.map(m => {
-    const match = m.match(/Property (\w+) As ([\w.]+)/);
-    return match ? `${match[1]}: ${match[2]}` : '';
+    const match = m.match(/Property (\w+) As ([\w.%()]+)/);
+    if (match) {
+      const type = match[2].split('(')[0].replace(/%/g, '').replace(/\./g, '_');
+      return `${match[1]}: ${type}`;
+    }
+    return '';
   }) : [];
+  
   const parameters = parameterMatches ? parameterMatches.map(m => {
     const match = m.match(/Parameter (\w+) = "([^"]*)"/);
     return match ? `${match[1]}: String` : ''; // Default Type is String
   }) : [];
-  return [...properties, ...parameters];
+  
+  const indexes = indexMatches ? indexMatches.map(m => {
+    const match = m.match(/Index (\w+) On (\w+)/);
+    return match ? `Index ${match[1]} On ${match[2]}` : '';
+  }) : [];
+  
+  return [...properties, ...parameters, ...indexes];
 }
 
 export function extractMethods(data: string): string[] {
